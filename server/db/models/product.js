4ae3a11cb2dfb7dbc1be751cc5e7f4877bc17caf
montekaka <josh.chen.jia@gmail.com>
2018-11-29 22:37:42 -0800

    add create suggest after create product, and add destroy all products and suggests

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -11,6 +11,8 @@ const ProductModelSchema = new Schema({
 	createdDate: {type: Date, default: Date.now}	
 });
 
+const Product = mongoose.model('Product', ProductModelSchema);
+
 const SuggestModelSchema = new Schema({
 	productId: { type: Schema.Types.ObjectId, ref: 'Product' },
 	suggestProduct: ProductModelSchema,
@@ -19,7 +21,6 @@ const SuggestModelSchema = new Schema({
 	createdDate: {type: Date, default: Date.now}		
 })
 
-const Product = mongoose.model('Product', ProductModelSchema);
 const Suggest = mongoose.model('Suggest', SuggestModelSchema);
 
 const fetch = (req, res) => {
@@ -42,21 +43,46 @@ const create = (req, res) => {
 	var promise = product.save();
 
 	promise.then((p) => {
-	  res.json(p);
+	  //res.json(p);
+	  var _query = Product.where('_id').ne(p._id);
+	  var q = _query.exec();
+	  return q.then((result) => {
+		  return {
+		  	newProduct: p,
+		  	products: result
+		  };	  	
+	  })
 	})
-	// promise.then((p) => {
-	// 	Product.where('_id').ne(p._id)
-	// 	.exec((products) => {
+	.then((result) => {
+		let newProduct = result.newProduct;
+		let products = result.products;
+		var promisesSuggestions = [];
+		for (var i = 0; i < products.length; i++) {
+			let _product = products[i];
+			let score = stringSimilarity.compareTwoStrings(_product['name'], newProduct['name']);
+			let _suggestion1 = new Suggest({
+				productId: newProduct._id,
+				score: score,
+				suggestProduct: _product
+			});
 
-	// 	})
-	// })
-	// Product.create(item, (err, res) => {
-	// 	if(err) {
-	// 		callback(err, null);
-	// 	} else {
-	// 		callback(null, res);
-	// 	}
-	// });
+			let _suggestion2 = new Suggest({
+				productId: _product._id,
+				score: score,
+				suggestProduct: newProduct
+			}); 
+			var suggestion1 = _suggestion1.save();
+			var suggestion2 = _suggestion2.save();			
+			promisesSuggestions.push(suggestion1);
+			promisesSuggestions.push(suggestion2);
+		}
+		return promisesSuggestions;
+	})
+	.then((suggestions) => {
+		Promise.all(suggestions).then(() => {
+			res.json({'msg': 'done'});
+		})
+	});
 }
 
 const destroy = (req, res) => {
@@ -70,7 +96,16 @@ const destroy = (req, res) => {
 	});
 }
 
+const destroyAll = (req, res) => {
+	Product.deleteMany({}, () => {
+		Suggest.deleteMany({}, () => {
+			res.json({'msg': 'Delete all'});
+		})
+	})
+}
+
+module.exports.Suggest = Suggest;
 module.exports.create = create;
 module.exports.fetch = fetch;
 module.exports.destroy = destroy;
-
+module.exports.destroyAll = destroyAll;
